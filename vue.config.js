44a7dbaaf725d0882a8b2a081e3d6f39b7409f65
DkG-07 <dkg.0717@gmail.com>
2020-07-19 16:01:53 -0500

    Ajuste de ruta

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // publicPath: process.env.NODE_ENV === 'production' ? '' : '/'
-    publicPath: process.env.NODE_ENV === 'production' ? '/web_app/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '' : '/'
 }
